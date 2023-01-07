f243c437518bb7396565e76b426d41ce484aa14b
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-01-07 06:52:12 +0100

    updates hooks

--- evveland-marketplace/contracts/EvvelandMarketplace.sol
+++ evveland-marketplace/contracts/EvvelandMarketplace.sol
@@ -4,52 +4,43 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract EvvelandMarketplace is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    Pausable,
-    Ownable,
-    ERC721Burnable
-{
+contract EvvelandMarketplace is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    Counters.Counter private _itemsSold;
+
     Counters.Counter private _listedItems;
+    Counters.Counter private _tokenIds;
 
-    uint256 listingPrice = 0.025 ether;
+    uint256 public listingPrice = 0.025 ether;
 
-    mapping(string => bool) private _usedTokenURIs;
-    mapping(uint256 => MarketItem) private idToMarketItem;
+    mapping(string => bool) private _usedTokenURI;
+    mapping(uint256 => MarketItem) private _tokenIdToMarketItem;
 
     mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-    mapping(uint256 => uint256) private _idToOwnedIndex;
+    mapping(uint256 => uint256) private _tokenIdTOwnedIndex;
 
-    uint256[] private _allNfts;
-    mapping(uint256 => uint256) private _idToNftIndex;
+    uint256[] private _totalMarketItems;
+    mapping(uint256 => uint256) private _tokenIdToNFTIndex;
 
     struct MarketItem {
         uint256 tokenId;
         address payable creator;
         uint256 price;
         bool isListed;
-        uint256 refID;
     }
 
     event MarketItemCreated(
         uint256 indexed tokenId,
         uint256 price,
         address creator,
-        bool isListed,
-        uint256 refID
+        bool isListed
     );
 
-    constructor() ERC721("Evveland Marketplace", "EVVE") {}
+    constructor() ERC721("Evveland Metaverse Marketplace", "EVVE") {}
 
     /* Updates the listing price of the item in the contract */
     function updateListingPrice(uint256 _newListingPrice)
@@ -71,42 +62,53 @@ contract EvvelandMarketplace is
         return _listedItems.current();
     }
 
-    function tokenURIExists(string memory _uri) public view returns (bool) {
-        return _usedTokenURIs[_uri] == true;
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return _allNfts.length;
-    }
-
-    function tokenByIndex(uint256 index)
+    function checkTokenURIExists(string memory _uri)
         public
         view
-        override
-        returns (uint256)
+        returns (bool)
     {
-        require(index < totalSupply(), "Index out of bounds");
-        return _allNfts[index];
+        return _usedTokenURI[_uri] == true;
     }
 
-    function tokenOfOwnerByIndex(address owner, uint256 index)
+    function getTotalMarketItems() public view returns (uint256) {
+        return _totalMarketItems.length;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalMarketItems.length;
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "Index out of bounds");
+        return _totalMarketItems[index];
+    }
+
+    function getTokenByIndex(uint256 _index) public view returns (uint256) {
+        require(_index < totalSupply(), "Index out of bounds");
+        return _totalMarketItems[_index];
+    }
+
+    function getTokenOfOwnerByIndex(address owner, uint256 index)
         public
         view
-        override
         returns (uint256)
     {
         require(index < ERC721.balanceOf(owner), "Index out of bounds");
         return _ownedTokens[owner][index];
     }
 
-    function getAllNftsOnSale() public view returns (MarketItem[] memory) {
+    function getAllListedMarketItems()
+        public
+        view
+        returns (MarketItem[] memory)
+    {
         uint256 allItemsCounts = totalSupply();
         uint256 currentIndex = 0;
         MarketItem[] memory items = new MarketItem[](_listedItems.current());
 
         for (uint256 i = 0; i < allItemsCounts; i++) {
             uint256 tokenId = tokenByIndex(i);
-            MarketItem storage item = idToMarketItem[tokenId];
+            MarketItem storage item = _tokenIdToMarketItem[tokenId];
 
             if (item.isListed == true) {
                 items[currentIndex] = item;
@@ -117,146 +119,117 @@ contract EvvelandMarketplace is
         return items;
     }
 
-    function getOwnedNfts() public view returns (MarketItem[] memory) {
+    function getUserOwnedNfts() public view returns (MarketItem[] memory) {
         uint256 ownedItemsCount = ERC721.balanceOf(msg.sender);
         MarketItem[] memory items = new MarketItem[](ownedItemsCount);
 
         for (uint256 i = 0; i < ownedItemsCount; i++) {
-            uint256 tokenId = tokenOfOwnerByIndex(msg.sender, i);
-            MarketItem storage item = idToMarketItem[tokenId];
+            uint256 tokenId = getTokenOfOwnerByIndex(msg.sender, i);
+            MarketItem storage item = _tokenIdToMarketItem[tokenId];
             items[i] = item;
         }
 
         return items;
     }
 
-    /* Mints a new token and lists it in the marketplace */
-    function mintToken(string memory _tokenURI, uint256 price)
+    /** Function to create and list an NFT on the marketplace */
+    function createMarketItem(string memory _uri, uint256 price)
         public
         payable
         returns (uint256)
     {
-        require(!tokenURIExists(_tokenURI), "Token URI already exists");
+        require(!checkTokenURIExists(_uri), "Token URI already exists");
         require(
             msg.value == listingPrice,
-            "Price must be equal to listing price"
+            "Listing Fee supplied must be equal to listing price"
         );
-
         _tokenIds.increment();
-        _listedItems.increment();
 
         uint256 newTokenId = _tokenIds.current();
 
         _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, _tokenURI);
-        _createNftItem(newTokenId, price);
-        _usedTokenURIs[_tokenURI] = true;
+        _setTokenURI(newTokenId, _uri);
+        _mintMarketItem(newTokenId, price);
+        _usedTokenURI[_uri] = true;
+        _totalMarketItems.push(newTokenId);
+
+        _listedItems.increment();
 
         return newTokenId;
     }
 
-    function _createNftItem(uint256 tokenId, uint256 price) private {
+    function _mintMarketItem(uint256 tokenId, uint256 price) private {
         require(price > 0, "Price must be at least 1 wei");
 
-        idToMarketItem[tokenId] = MarketItem(
+        _tokenIdToMarketItem[tokenId] = MarketItem(
             tokenId,
             payable(msg.sender),
             price,
-            true,
-            0
+            true
         );
 
-        emit MarketItemCreated(tokenId, price, msg.sender, true, 0);
+        emit MarketItemCreated(tokenId, price, msg.sender, true);
     }
 
-    function buyNft(uint256 tokenId) public payable {
-        uint256 price = idToMarketItem[tokenId].price;
-        address owner = ERC721.ownerOf(tokenId);
-
-        require(msg.sender != owner, "You already own this NFT");
-        require(msg.value == price, "Please submit the asking price");
-
-        idToMarketItem[tokenId].isListed = false;
-        _listedItems.decrement();
-
-        _transfer(owner, msg.sender, tokenId);
-        payable(owner).transfer(msg.value);
-    }
-
-    function placeNftOnSale(uint256 tokenId, uint256 newPrice) public payable {
-        require(
-            ERC721.ownerOf(tokenId) == msg.sender,
-            "You are not owner of this nft"
-        );
-        require(
-            idToMarketItem[tokenId].isListed == false,
-            "Item is already on sale"
-        );
-        require(
-            msg.value == listingPrice,
-            "Price must be equal to listing price"
-        );
-
-        idToMarketItem[tokenId].isListed = true;
-        idToMarketItem[tokenId].price = newPrice;
-        _listedItems.increment();
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function safeMint(
-        address to,
-        uint256 tokenId,
-        string memory uri
-    ) public onlyOwner {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
+    // The following functions are overrides required by Solidity.
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId,
         uint256 batchSize
-    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+    ) internal virtual override {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
+
+        if (from == address(0)) {
+            _addTokenToAllTokensEnumeration(tokenId);
+        } else if (from != to) {
+            _removeTokenFromOwnerEnumeration(from, tokenId);
+        }
+
+        if (to == address(0)) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        } else if (to != from) {
+            _addTokenToOwnerEnumeration(to, tokenId);
+        }
     }
 
-    // The following functions are overrides required by Solidity.
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _tokenIdToNFTIndex[tokenId] = _totalMarketItems.length;
+        _totalMarketItems.push(tokenId);
+    }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        uint256 length = ERC721.balanceOf(to);
+        _ownedTokens[to][length] = tokenId;
+        _tokenIdTOwnedIndex[tokenId] = length;
+    }
+
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId)
+        private
     {
-        super._burn(tokenId);
+        uint256 lastTokenIndex = ERC721.balanceOf(from) - 1;
+        uint256 tokenIndex = _tokenIdTOwnedIndex[tokenId];
+
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+            _ownedTokens[from][tokenIndex] = lastTokenId;
+            _tokenIdTOwnedIndex[lastTokenId] = tokenIndex;
+        }
+
+        delete _tokenIdTOwnedIndex[tokenId];
+        delete _ownedTokens[from][lastTokenIndex];
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+        uint256 lastTokenIndex = _totalMarketItems.length - 1;
+        uint256 tokenIndex = _tokenIdToNFTIndex[tokenId];
+        uint256 lastTokenId = _totalMarketItems[lastTokenIndex];
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+        _totalMarketItems[tokenIndex] = lastTokenId;
+        _tokenIdToNFTIndex[lastTokenId] = tokenIndex;
 
-    function withdraw(address _addr) external onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(_addr).transfer(balance);
+        delete _tokenIdToNFTIndex[tokenId];
+        _totalMarketItems.pop();
     }
 }
